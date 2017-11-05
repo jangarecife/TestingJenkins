2774778de3de7495b9d2032ae9f3b8e442df5ef2
JangeRecife <jangarecife@gmail.com>
2017-11-05 19:21:49 +0100

    10 to 5

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 package hello;
 public class Hello {
 	public static void main(String[] args) {
-		for (int i = 1 ; i<=10; i++)
+		for (int i = 1 ; i<=5; i++)
 			System.out.println("Hello 1 World " +i);
 	}
 }
