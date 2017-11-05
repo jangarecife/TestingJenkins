521db554e7f02c43bfacf7ffd9b6c6a14dd2243b
JangeRecife <jangarecife@gmail.com>
2017-11-05 19:15:56 +0100

    new

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ package hello;
 public class Hello {
 	public static void main(String[] args) {
 		for (int i = 1 ; i<=10; i++)
-			System.out.println("Hello One World " +i);
+			System.out.println("Hello 1 World " +i);
 	}
 }
